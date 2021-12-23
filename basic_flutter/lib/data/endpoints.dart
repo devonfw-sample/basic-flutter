2ee5f38eeea5077f3c93077d8d278c2c8a09f6f7
HKayed <90037037+HKayed@users.noreply.github.com>
2021-12-23 12:40:38 +0100

    endpoints change

--- basic_flutter/lib/data/endpoints.dart
+++ basic_flutter/lib/data/endpoints.dart
@@ -1,6 +1,8 @@
 class Endpoints {
   static const searchEmployeeListEndpoint =
-      "http://localhost:8080/employeemanagement/v1/employee/search/";
+      'https://localhost:8080/employeemanagement/v1/employee/search/';
+  static const getEmployeeWithIdEndpoint =
+      'https://localhost:8080/employeemanagement/v1/employee/';
   static const String deleteEmployeeEndpoint =
-      'https://virtserver.swaggerhub.com/flutterteam2/flutter/1.0.0/employeemanagement/v1/employee/';
+      'https://localhost:8080/employeemanagement/v1/employee/';
 }
