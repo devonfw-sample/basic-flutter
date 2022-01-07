841ebb14a41860986fed25ea42be2c42de96c198
HKayed <90037037+HKayed@users.noreply.github.com>
2022-01-07 21:42:31 +0100

    initial modifications

---
+++ basic_flutter/lib/business_logic/cubits/employee_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:bloc/bloc.dart';
+import 'employee_state.dart';
+
+class EmployeeCubit extends Cubit<EmployeeState> {
+  EmployeeCubit(EmployeeState initialState) : super(initialState);
+}
