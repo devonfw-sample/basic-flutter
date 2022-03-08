a650216e9be31bfc348dd8f39467646188d93f17
TugbaDalmaz <tugba.dalmaz@capgemini.com>
2022-03-08 14:27:33 +0100

    splash_screen

--- basic_flutter/lib/main.dart
+++ basic_flutter/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-import '/dummy_page.dart';
 import 'package:flutter/material.dart';
+import './screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,55 +21,6 @@ class MyApp extends StatelessWidget {
               headline6:
                   const TextStyle(fontFamily: "Raleway-Bold", fontSize: 20),
             ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-      const Duration(seconds: 3),
-      () {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (_) => const DummyPage(),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue.shade900,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage('assets/logo.png'),
-              width: 300,
-              height: 300,
-            ),
-            const SizedBox(height: 20),
-            const CircularProgressIndicator(
-              strokeWidth: 5,
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-            ),
-          ],
-        ),
       ),
     );
   }
