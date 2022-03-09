21957073186028f12b1670f0cfb204a29f852714
HKayed <90037037+HKayed@users.noreply.github.com>
2022-03-09 11:52:57 +0100

    server bugs fixed

--- basic_flutter/server/service/UpdateEmployeeService.js
+++ basic_flutter/server/service/UpdateEmployeeService.js
@@ -18,6 +18,19 @@ exports.employeemanagementV1EmployeeIdPUT = function(body,id) {
   "id" : body.id,
   "email" : body.email
 };
+for(var i = 0; i < data.length; i++){
+  if(data[i] && data[i].id == id){
+    data[i] = {
+      "surname" : body.surname,
+      "modificationCounter" : body.modificationCounter,
+      "name" : body.name,
+      "employeeId" : body.employeeId,
+      "id" : body.id,
+      "email" : body.email
+    };
+    break;
+  }
+}
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
